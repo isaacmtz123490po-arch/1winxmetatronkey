83eaddfdbe84cdd35928f651af3f2aa8968e3a5c
isaacmtz123490po-arch <isaacmtz123490po@gmail.com>
2026-08-16 18:38:13 -0600

    Actualizar settings.py

--- settings.py
+++ settings.py
@@ -5,8 +5,8 @@
 import os
 from pathlib import Path
 # Proxy and Web GUI
-PROXY_HOST = '0.0.0.0'
-PROXY_PORT = 1337
+PROXY_HOST = '192.168.1.66'
+PROXY_PORT = 8888
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 HOME_DIR = os.path.join(str(Path.home()), '.httptools')
 FLOWS_DIR = os.path.join(HOME_DIR, 'flows')
